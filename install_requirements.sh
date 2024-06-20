aa08a5a9b1a4c750c84ae9ec6fe9a72d33d48794
sm <entertainmenthodd@gmail.com>
2024-06-20 09:49:40 +0200

    updated installer

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
 # Script to quickly install all pip dependencies
+
+# Check if python is installed
+if ! [ -x "$(command -v python)" ]; then
+  echo 'Error: python is not installed.' >&2
+  exit 1
+fi
+
+# Check if pip is installed
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Error: pip is not installed.' >&2
+  exit 1
+fi
+
+echo "Installing all dependencies..."
 pip install --break-system-packages -r requirements.txt
